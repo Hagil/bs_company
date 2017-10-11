bd0f00a858acd85f5f58b1cbbf65a6b7805022da
Hagil Baik <hagilbaik@gmail.com>
2017-10-11 15:52:20 +0800

    VICARIOUSLY experience some reason to LIVE!!

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,13 @@
-console log("loaded");
+console.log("loaded");
 //AIzaSyBYnvDLc4VVOaopd1lK-CLNF_t2C41TsC8
+function initMap() {
+        var gbi = {lat: 22.375057, lng: 114.110579};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: gbi
+        });
+        var marker = new google.maps.Marker({
+          position: gbi,
+          map: map
+        });
+      }
