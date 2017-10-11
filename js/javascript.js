5095dd54de5ca0c1f771b20df58429069318911c
Hagil Baik <hagilbaik@gmail.com>
2017-10-11 16:25:50 +0800

    It's clever, but is it art?

--- js/javascript.js
+++ js/javascript.js
@@ -1,27 +1,40 @@
 console.log("loaded");
 //AIzaSyBYnvDLc4VVOaopd1lK-CLNF_t2C41TsC8
 function initMap() {
-        var gbi = {lat: 22.375057, lng: 114.110579};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 17,
-          center: gbi
-        });
-        var marker = new google.maps.Marker({
-          position: gbi,
-          map: map
-        });
-      }
-
-      $(document).ready(setup);
-
-      function setup(){
-      	console.log('Inside setup');
-      	$('#send').click(send_data);
-      }
-      function send_data(){
-      	console.log('Inside send_data');
-      	var name =$('#name').val();
-      	var email =$('#email').val();
-      	var comments =$('#comments').val();
-      	console.log('data:' + name + ', ' + email + ', ' + comments);
-      }
+var gbi = {lat: 22.375057, lng: 114.110579};
+var map = new google.maps.Map(document.getElementById('map'), {
+zoom: 17,
+center: gbi
+});
+var marker = new google.maps.Marker({
+position: gbi,
+map: map
+});
+}
+$(document).ready(setup);
+function setup(){
+console.log('Inside setup');
+$('#send').click(send_data);
+}
+function send_data(){
+console.log('Inside send_data');
+var name =$('#name').val();
+var email =$('#email').val();
+var comments =$('#comments').val();
+//console.log('data:' + name + ', ' + email + ', ' + comments);
+var contact =  {
+name: name,
+email: email,
+comments: comments
+};
+console.log(contact);
+$.get('results.php', contact).done(success).fail(oops);
+}
+function success(data){
+console.log('Inside success');
+console.log(data);
+}
+function oops(data){
+console.log('Inside oops');
+console.log(data);
+}
