64ce625a7e68e058e42e9c937a4b01f2feaa397c
Hagil Baik <hagilbaik@gmail.com>
2017-10-11 16:09:05 +0800

    THEGODDESSOFTHENETHASTWISTINGFINGERSANDHERVOICEISLIKEAJAVELININTHENIGHTDUDE

--- js/javascript.js
+++ js/javascript.js
@@ -11,3 +11,17 @@ function initMap() {
           map: map
         });
       }
+
+      $(document).ready(setup);
+
+      function setup(){
+      	console.log('Inside setup');
+      	$('#send').click(send_data);
+      }
+      function send_data(){
+      	console.log('Inside send_data');
+      	var name =$('#name').val();
+      	var email =$('#email').val();
+      	var comments =$('#comments').val();
+      	console.log('data:' + name + ', ' + email + ', ' + comments);
+      }
